f03469680cd1607b2ca8e30c54d2d95f639464b4
Palaash Kolhe <kolhepalaash02@gmail.com>
2022-12-03 23:18:03 -0500

    initialized connection between client and the server

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { React, Component } from 'react';
+import Web3 from 'web3';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Contract, ethers } from "ethers";
+import './App.css';
+import ExchangeContract from "./contracts/Exchange.json";
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      exchange: null,
+      account: null,
+      creditArray: [],
+    }
+  }
+
+  componentDidMount() {
+    this.loadSmartContract();
+  }
+
+  async loadCredits() {
+    const credit = await this.state.exchange.functions.getCreditArray();
+    this.setState({ creditArray: credit }, () => { console.log(this.state) });
+  }
+
+  async loadSmartContract() {
+    try {
+      const { ethereum } = window
+
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const exchange = new ethers.Contract(
+          ExchangeContract.networks[5777].address,
+          ExchangeContract.abi,
+          signer
+        )
+        const accounts = await provider.listAccounts();
+        this.setState({ exchange: exchange, account: accounts[0] }, () => this.loadCredits());
+      } else {
+        console.log("Ethereum object doesn't exist");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+        hello your account is: {this.state.account} and your contract address is:
+      </div>
+    )
+  }
 }
 
 export default App;
