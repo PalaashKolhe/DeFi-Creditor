7861a0d2643a53d8a9dc628e9fbadf1fe5f5fab1
Palaash Kolhe <kolhepalaash02@gmail.com>
2022-12-21 10:37:24 -0700

    working on front end to display credits

--- client/src/components/MainPage/Credit/Credit.js
+++ client/src/components/MainPage/Credit/Credit.js
@@ -4,21 +4,40 @@ import "./Credit.css";
 export default class Credit extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            usdPrice: 0,
+        }
+    }
+
+    componentDidMount() {
+        if (this.props.usdToEthMultiplier) {
+            var usdPrice = parseFloat(this.props.amount) * this.props.usdToEthMultiplier;
+            this.setState({ usdPrice: usdPrice.toFixed(2) })
+        }
     }
 
     render() {
         return (
-            <div className="modal frcc">
-                <div>
-                    Address: {this.props.borrower}
+            <div className="modal frbc">
+                <div className="fccs">
+                    <div className="modal-title">
+                        Address
+                    </div>
+                    <div>
+                        {this.props.borrower}
+                    </div>
                 </div>
                 {/* <div className="fccc">
                     <div>Description:</div>
                     <div>{this.props.descriptions}</div>                
                 </div> */}
-                <div>
-                    Amount: {this.props.amount} ETH
+                <div className="fcce">
+                    <div className="modal-title">
+                        Amount
+                    </div>
+                    <div>
+                        {this.props.amount} ETH (${this.state.usdPrice})
+                    </div>
                 </div>
             </div>
         )
--- client/src/components/MainPage/MainPage.js
+++ client/src/components/MainPage/MainPage.js
@@ -3,12 +3,14 @@ import Header from "../Header/Header";
 import "./MainPage.css";
 import "../../global.css";
 import Credit from "./Credit/Credit";
+import axios from 'axios';
 
 export default class MainPage extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            creditArray: []
+            creditArray: [],
+            usdToEthMultiplier: 1,
         }
     }
 
@@ -20,6 +22,15 @@ export default class MainPage extends Component {
 
     componentDidMount() {
         this.getCreditInformation();
+        this.convertEthToUsd();
+    }
+
+    async convertEthToUsd(eth) {
+        await axios.get("https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=ETH,USD").then((response) => {
+            this.setState({ usdToEthMultiplier: response.data["USD"] })
+        }).catch(() => {
+            this.setState({ usdToEthMultiplier: 0 });
+        })
     }
 
     async getCreditInformation() {
@@ -39,18 +50,22 @@ export default class MainPage extends Component {
     }
 
     render() {
+        var usdToEthMultiplier = this.state.usdToEthMultiplier;
+        
         return (
-            <div className="landingPage">
+            <div className="landing-page">
                 <Header />
                 <div className="credit">
                     <p align="left">
                         DeFi Creditor introduces collateral-free and trust-less lending on the blockchain, maximizing capital efficiency for borrowers and interest returns for lenders. These users below could use your help:
                     </p>
-                    {this.state.creditArray.map(function (credit, i) {
-                        return (
-                            <Credit key={i} borrower={credit["borrower"]} amount={credit["amount"]} />
-                        )
-                    })}
+                    <div className="modal-container">
+                        {this.state.creditArray.map(function (credit, i) {
+                            return (
+                                <Credit key={i} usdToEthMultiplier={usdToEthMultiplier} borrower={credit["borrower"]} amount={credit["amount"]} />
+                            )
+                        })}
+                    </div>
                 </div>
             </div>
         )
